f713cb67085727af038cb83681d36e8a3dfc9f31
Voxun <43830043+anguy258@users.noreply.github.com>
2021-10-17 19:06:57 -0700

    Deploy to Heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 const port = process.env.PORT || 3000
 
 app.get('/*', (req, res) => {
-    res.sendFile('./client/index.html', {root: './'})
+    // res.sendFile('./client/index.html', {root: './'})
     res.redirect('./')
 })
 
